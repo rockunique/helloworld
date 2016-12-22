95477af18a562d78657a332ed8fc5345e7bbdd28
chris <chris@chriss-iMac.local>
2016-12-22 15:34:20 +0000

    Conflict with Richard's commit?

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Chris's comment'
     }
 
 
