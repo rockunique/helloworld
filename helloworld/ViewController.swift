0492c150e4eb087765bfe9293ef0e7dc089250f4
chris <chris@chriss-iMac.local>
2016-12-22 15:28:49 +0000

    A third comment

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     //Hi Richard!
     // Added another comment
+    // A third comment
 
     override func viewDidLoad() {
         super.viewDidLoad()
