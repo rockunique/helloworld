7036c0fafb278e3ccfabacca1c455f6ccc338c20
Richard Hammond <richammond@Richards-MacBook-Air.local>
2016-12-22 14:01:16 +0000

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Richard Hammond on 22/12/2016.
+//  Copyright © 2016 Rock Unique Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
